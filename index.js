bf1a568b38512c936c364ffc5cc8cef5d1d17fd1
Gustavo Sampaio <gbritosampaio@gmail.com>
2015-10-26 13:59:44 -0300

    load/save methods

--- index.js
+++ index.js
@@ -2,16 +2,39 @@ var EventEmitter = require('events').EventEmitter;
 var deck = require('deck');
 var Lazy = require('lazy');
 var Hash = require('hashish');
+var fs = require('fs');
 
 module.exports = function (order) {
     if (!order) order = 2;
     var db = {};
     var self = {};
-    
+
+    self.save = function (filename, cb) {
+      var data = JSON.stringify({
+          version: '1',
+          order: order,
+          db: db
+        });
+      fs.writeFile(filename, data, function(err) {
+        if(err) {
+          console.log(err);
+          if(cb) cb(err);
+        }
+        if(cb) cb();
+      });
+    };
+
+    self.load = function (filename, cb) {
+      var model = JSON.parse(fs.readFileSync(filename, 'utf-8'));
+      order = model.order;
+      db = model.db;
+      if(cb) cb();
+    }
+
     self.seed = function (seed, cb) {
         if (seed instanceof EventEmitter) {
             Lazy(seed).lines.forEach(self.seed);
-            
+
             if (cb) {
                 seed.on('error', cb);
                 seed.on('end', cb);
@@ -21,23 +44,23 @@ module.exports = function (order) {
             var text = (Buffer.isBuffer(seed) ? seed.toString() : seed)
             var words = text.split(/\s+/);
             var links = [];
-            
+
             for (var i = 0; i < words.length; i += order) {
                 var link = words.slice(i, i + order).join(' ');
                 links.push(link);
             }
-            
+
             if (links.length <= 1) {
                 if (cb) cb(null);
                 return;
             }
-            
+
             for (var i = 1; i < links.length; i++) {
                 var word = links[i-1];
                 var cword = clean(word);
                 var next = links[i];
                 var cnext = clean(next);
-                
+
                 var node = Hash.has(db, cword)
                     ? db[cword]
                     : {
@@ -48,7 +71,7 @@ module.exports = function (order) {
                     }
                 ;
                 db[cword] = node;
-                
+
                 node.count ++;
                 node.words[word] = (
                     Hash.has(node.words, word) ? node.words[word] : 0
@@ -66,7 +89,7 @@ module.exports = function (order) {
                     node.prev[''] = (node.prev[''] || 0) + 1;
                 }
             }
-            
+
             if (!Hash.has(db, cnext)) db[cnext] = {
                 count : 1,
                 words : {},
@@ -77,14 +100,14 @@ module.exports = function (order) {
             n.words[next] = (Hash.has(n.words, next) ? n.words[next] : 0) + 1;
             n.prev[cword] = (Hash.has(n.prev, cword) ? n.prev[cword] : 0) + 1;
             n.next[''] = (n.next[''] || 0) + 1;
-            
+
             if (cb) cb(null);
         }
     };
-    
+
     self.search = function (text) {
         var words = text.split(/\s+/);
-        
+
         // find a starting point...
         var start = null;
         var groups = {};
@@ -92,34 +115,34 @@ module.exports = function (order) {
             var word = clean(words.slice(i, i + order).join(' '));
             if (Hash.has(db, word)) groups[word] = db[word].count;
         }
-        
+
         return deck.pick(groups);
     };
-    
+
     self.pick = function () {
         return deck.pick(Object.keys(db))
     };
-    
+
     self.next = function (cur) {
         if (!cur || !db[cur]) return undefined;
-        
+
         var next = deck.pick(db[cur].next);
         return next && {
             key : next,
             word : deck.pick(db[next].words),
         } || undefined;
     };
-    
+
     self.prev = function (cur) {
         if (!cur || !db[cur]) return undefined;
-        
+
         var prev = deck.pick(db[cur].prev);
         return prev && {
             key : prev,
             word : deck.pick(db[prev].words),
         } || undefined;
     };
-    
+
     self.forward = function (cur, limit) {
         var res = [];
         while (cur && !limit || res.length < limit) {
@@ -128,10 +151,10 @@ module.exports = function (order) {
             cur = next.key;
             res.push(next.word);
         }
-        
+
         return res;
     };
-    
+
     self.backward = function (cur, limit) {
         var res = [];
         while (cur && !limit || res.length < limit) {
@@ -140,18 +163,18 @@ module.exports = function (order) {
             cur = prev.key;
             res.unshift(prev.word);
         }
-        
+
         return res;
     };
-    
+
     self.fill = function (cur, limit) {
         var res = [ deck.pick(db[cur].words) ];
         if (!res[0]) return [];
         if (limit && res.length >= limit) return res;;
-        
+
         var pcur = cur;
         var ncur = cur;
-        
+
         while (pcur || ncur) {
             if (pcur) {
                 var prev = self.prev(pcur);
@@ -162,7 +185,7 @@ module.exports = function (order) {
                     if (limit && res.length >= limit) break;
                 }
             }
-            
+
             if (ncur) {
                 var next = self.next(ncur);
                 ncur = null;
@@ -173,19 +196,19 @@ module.exports = function (order) {
                 }
             }
         }
-        
+
         return res;
     };
-    
+
     self.respond = function (text, limit) {
         var cur = self.search(text) || self.pick();
         return self.fill(cur, limit);
     };
-    
+
     self.word = function (cur) {
         return db[cur] && deck.pick(db[cur].words);
     };
-    
+
     return self;
 };
 
